0fb680d4c13c53527b64a89ecbdf463b93e037ab
Peiyi Xu <xu_py311@outlook.com>
2023-05-28 11:20:18 -0400

    finished ex1.35

---
+++ chapter1/section3/fixed-point.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? next guess)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+(fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (/ (+ y (/ x y)) 2)) 1.0))
+
+(sqrt 2)

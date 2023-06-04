2b45e2ba19d94cf008f6ce6a0355d173c255ac4d
Peiyi Xu <xu_py311@outlook.com>
2023-06-03 23:29:22 -0400

    finished 2.17

---
+++ chapter2/section1/exercise11.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (sub-interval x y)
+  (add-interval x (make-interval (- (upper-bound y))
+                                 (- (lower-bound y)))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (= (span y) 0)
+      (error "Cannot divide an interval of 0 span")
+      (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+                                 (/ 1.0 (lower-bound y))))))
+
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound interval) (max (car interval) (cdr interval)))
+
+(define (lower-bound interval) (min (car interval) (cdr interval)))
+
+(define (span interval) (- (upper-bound interval) (lower-bound interval)))

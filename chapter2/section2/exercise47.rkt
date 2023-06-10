3a9a7fe21548ef82a8893292ca6fb9a43c0b9855
Peiyi Xu <xu_py311@outlook.com>
2023-06-09 23:14:10 -0400

    finished 2.47

---
+++ chapter2/section2/exercise47.rkt
@@ -0,0 +1,26 @@
+#lang racket
+;;a
+#|
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (caddr frame))
+|#
+
+
+;; b
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (cddr frame))

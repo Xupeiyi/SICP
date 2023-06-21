9c2c04349e4952074dbc4ea289b9fff9faff5aa3
Peiyi Xu <xu_py311@outlook.com>
2023-06-20 22:39:20 -0400

    finished 2.68

--- chapter2/section3/exercise68.rkt
+++ chapter2/section3/exercise68.rkt
@@ -42,6 +42,27 @@
               (decode-1 (cdr bits) next-branch)))))
   (decode-1 bits tree))
 
+;; encode symbol
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (encode-symbol symbol tree)
+  (define (encode-1 current-branch result)
+    (if (leaf? current-branch) result
+        (let ((left (left-branch current-branch))
+              (right (right-branch current-branch)))
+            (cond ((element-of-set? symbol (symbols left)) (encode-1 left (append result '(0))))
+                  ((element-of-set? symbol (symbols right)) (encode-1 right (append result '(1))))
+                  (else (error "INVALID SYMBOL -- " symbol))))))
+  (encode-1 tree '()))
+  
+;; encode
+(define (encode message tree)
+  (if (null? message) '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
 
 (define sample-tree
   (make-code-tree (make-leaf 'A 4)
@@ -49,6 +70,10 @@
                                   (make-code-tree (make-leaf 'D 1)
                                                   (make-leaf 'C 1)))))
 
-(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+(define sample-message '(A D A B B C A))
 
-(decode sample-message sample-tree) ;;'(A D A B B C A)
+(decode (encode sample-message sample-tree) sample-tree) ;;'(A D A B B C A)
+ 
+
+
+

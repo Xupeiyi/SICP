22e06b91b8ad99668f0fcba7321efe832e5caf9e
Peiyi Xu <xu_py311@outlook.com>
2023-07-01 17:05:14 -0400

    finished 2.11

--- chapter2/section1/exercise11.rkt
+++ chapter2/section1/exercise11.rkt
@@ -7,12 +7,31 @@
   (add-interval x (make-interval (- (upper-bound y))
                                  (- (lower-bound y)))))
 
+(define (interval-positive? interval) (>= (lower-bound interval) 0))
+
+(define (interval-cross-zero? interval) (and (< (lower-bound interval) 0) (>= (upper-bound interval) 0)))
+
+(define (interval-negative? interval) (< (upper-bound interval) 0))
+
 (define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-        (p2 (* (lower-bound x) (upper-bound y)))
-        (p3 (* (upper-bound x) (lower-bound y)))
-        (p4 (* (upper-bound x) (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
+  (let ((a (lower-bound x))
+        (b (upper-bound x))
+        (c (lower-bound y))
+        (d (upper-bound y)))
+        (cond ((interval-positive? x) 
+               (cond ((interval-positive? y) (make-interval (* a c) (* b d)))
+                     ((interval-cross-zero? y) (make-interval (* b c) (* b d)))                      
+                     ((interval-negative? y) (make-interval (* b c) (* a d)))))
+                    
+              ((interval-cross-zero? x)
+               (cond ((interval-positive? y) (make-interval (* a d) (* b d)))
+                     ((interval-cross-zero? y) (make-interval (min (* a d) (* b c)) 
+                                                              (max (* a c) (* b d))))
+                     ((interval-negative? y) (make-interval (* b c) (* a c)))))
+              ((interval-negative? x) 
+               (cond ((interval-positive? y) (make-interval (* a d) (* b c)))
+                     ((interval-cross-zero? y) (make-interval (* a d) (* a c)))
+                     ((interval-negative? y) (make-interval (* b d) (* a c))))))))
 
 (define (div-interval x y)
   (if (= (span y) 0)
@@ -27,3 +46,7 @@
 (define (lower-bound interval) (min (car interval) (cdr interval)))
 
 (define (span interval) (- (upper-bound interval) (lower-bound interval)))
+
+(let ((i1 (make-interval -50 10))
+      (i2 (make-interval -20 30)))
+  (mul-interval i1 i2))

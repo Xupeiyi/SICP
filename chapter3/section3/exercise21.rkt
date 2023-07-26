4d53ab067d730fc2d9bf289712b1ffd673e07dc1
Peiyi Xu <xu_py311@outlook.com>
2023-07-25 23:02:43 -0400

    finished 3.21

--- chapter3/section3/exercise21.rkt
+++ chapter3/section3/exercise21.rkt
@@ -1,3 +1,5 @@
+#lang racket
+
 (define (front-ptr queue) (mcar queue))
 
 (define (rear-ptr queue) (mcdr queue))
@@ -8,15 +10,15 @@
 
 (define (empty-queue? queue) (null? (front-ptr queue)))
 
-(define (make-queue) (cons '() '()))
+(define (make-queue) (mcons '() '()))
 
 (define (front-queue queue) 
     (if (empty-queue? queue)
         (error "FRONT called with an empty queue " queue)
-        (car (front-ptr queue))))
+        (mcar (front-ptr queue))))
 
 (define (insert-queue! queue item) 
-    (let ((new-pair (cons item '()))
+    (let ((new-pair (mcons item '())))
         (cond ((empty-queue? queue) 
                (set-front-ptr! queue new-pair)
                (set-rear-ptr! queue new-pair)
@@ -24,11 +26,40 @@
               (else 
                 (set-mcdr! (rear-ptr queue) new-pair)
                 (set-rear-ptr! queue new-pair)
-                queue)))))
+                queue))))
 
-(define (delete-queue! queue item)
+(define (delete-queue! queue)
     (cond ((empty-queue? queue) 
             (error "DELETE! called with an empty queue"))
           (else 
-            (set-front-ptr! queue (cdr (front-ptr queue)))
+            (set-front-ptr! queue (mcdr (front-ptr queue)))
             queue)))
+
+(define (print-queue queue)
+  (define (print-list list) 
+    (unless (null? list) 
+      (begin (display (mcar list)) (display " ") 
+             (print-list (mcdr list)))))
+  (if (empty-queue? queue)
+      (print null)
+      (begin (display "Queue: ")
+             (print-list (front-ptr queue)) 
+             (display "\n"))))
+
+; feels like cheating to use the REPL to print the list
+; (define (print-queue q)
+;   (print (front-ptr q)))
+
+(define q1 (make-queue))
+; (print-queue q1)
+(insert-queue! q1 'a)
+(insert-queue! q1 'b)
+(insert-queue! q1 'c)
+(print-queue q1)
+
+(delete-queue! q1)
+(delete-queue! q1)
+(print-queue q1)
+
+(delete-queue! q1)
+(print-queue q1)

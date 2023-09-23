e1445c3ecf85a4abce3bbb3876d4b1479ce7abd7
Peiyi Xu <xu_py311@outlook.com>
2023-09-23 15:03:27 -0400

    finished 4.1

---
+++ chapter4/section1/exercise1.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (list-of-values-left-to-right exps env)
+    (if (no-operands? exps)
+        '()
+        (let ((left (eval (first-operand exps) env)))
+            (cons left 
+                  (list-of-values (rest-operands exps) env)))))
+
+(define (list-of-values-right-to-left exps env) 
+    (if (no-operands? exps)
+        '()
+        (let ((right (list-of-values (rest-operands exps) env)))
+             (cons (eval (first-operand exps) env) 
+                   right))))

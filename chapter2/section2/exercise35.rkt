21a482e18e9571402b7792a775ff2fb4aadcb8e3
Peiyi Xu <xu_py311@outlook.com>
2023-09-30 09:47:10 -0400

    finished 3.29

--- chapter2/section2/exercise35.rkt
+++ chapter2/section2/exercise35.rkt
@@ -16,6 +16,10 @@
 (define tree (list (list 1 2 3 4) (list (list (list 5) 6 7 (list 8 9)))))
 
 (define (count-leaves t)
-  (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1)) t)))
+  (accumulate + 
+              0 
+              (map (lambda (x) 
+                           (if (pair? x) (count-leaves x) 1)) t)))
 
 (count-leaves tree)
+(count-leaves '())

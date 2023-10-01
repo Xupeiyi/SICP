cc66c4fa3beed66fc0bd3430e0b61e32e73a8d8e
Peiyi Xu <xu_py311@outlook.com>
2023-10-01 15:25:34 -0400

    finished 3.31

---
+++ chapter3/section3/exercise31.rkt
@@ -0,0 +1,2 @@
+;; For an inverter, if invert-input is not run immediately, the after-delay method won't be called, 
+;; and the action would not be added to agenda, and won't be called when running propagate.

27e28281cafaa47a6492a17d890b14132c8132c8
Peiyi Xu <xu_py311@outlook.com>
2025-07-19 12:22:26 -0400

    added internal data structures to the evaluator

--- chapter4/section1/evaluator.rkt
+++ chapter4/section1/evaluator.rkt
@@ -196,3 +196,103 @@
                  (make-if (cond-predicate first)
                           (sequence->exp (cond-actions first))
                           (expand-clauses rest))))))
+
+;; predicates
+(define (true? x) 
+    (not (eq? x false)))
+
+(define (false? x) 
+    (eq? x false))
+
+;; application
+
+;; primitive-procedure
+;; (apply-primitive-procedure <proc> <args>)
+;; (premitive-procedure? <proc>)
+
+;; compound-procedure
+(define (make-procedure parameters body env) 
+    (list 'procedure parameters body env))
+
+(define (compound-procedure? p) 
+    (tagged-list? p 'procedure))
+
+(define (procedure-parameters p) (cadr p))
+
+(define (procedure-body p) (caddr p))
+
+(define (procedure-environment p) (cadddr p))
+
+;; environment
+;; (lookup-variable-value var env)
+;; (extend-environment <variables> <values> <base-env>)
+;; (define-variable! <var> <value> <env>)
+;; (set-variable-value! <var> <value> <env>)
+
+;; implement the environment as a list of frames
+(define (enclosing-environment env) (cdr env))
+
+(define (first-frame env) (car env))
+
+(define the-empty-environment '())
+
+;; frame
+(define (make-frame variables values)
+    (cons variables values))
+
+(define (frame-variables frame) (car frame))
+
+(define (frame-values frame) (cdr frame))
+
+(define (add-binding-to-frame! var val frame) 
+    (set-car! frame (cons var (car frame)))  ;; (cons var (frame-variables frame))
+    (set-cdr! frame (cons val (cdr frame)))) ;; (cons val (frame-values frame))
+
+(define (extend-environment vars vals base-env) 
+    (if (= (length vars) (length vals))
+        (cons (make-frame vars vals) base-env)
+        (if (< (length vars) (length vals))
+            (error "Too many arguments supplied" vars vals)
+            (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+    (define (env-loop env)
+        (define (scan vars vals)
+            (cond ((null? vars) 
+                   (env-loop (enclosing-environment env)))
+                  ((eq? var (car var))  
+                   (car vals))
+                  (else 
+                   (scan (car vars) (car vals)))))
+        (if (eq? env the-empty-environment)
+            (error "Unbound variable" var)
+            (let ((frame (first-frame env))
+                 (scan (frame-variables frame)
+                       (frame-values frame))))))     
+    (env-loop env))
+
+(define (set-variable-value! var val env)
+    (define (env-loop env)
+        (define (scan vars vals)
+            (cond ((null? vars)
+                   (env-loop (enclosing-environment env)))
+                  ((eq? var (car vars))
+                   (set-car! vals val))
+                  (else
+                   (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let ((frame (first-frame env)))
+             (scan (frame-variables frame)
+                   (frame-values frame)))))
+    (env-loop env))
+
+(define (define-variable! var val env)
+    (let ((frame (first-frame env)))
+        (define (scan vars vals)
+            (cond ((null? vars)
+                   (add-binding-to-frame! var val frame))
+                  ((eq? var (car vars))
+                   (set-car! vals val))
+                  (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame) (frame-values frame))))
